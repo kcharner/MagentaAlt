b76f8e4b27ba2dda382aa42f5a2bd0bbda8b40a8
Ishani Amin <ishaniamin@Ishanis-MacBook-Pro.local>
2017-03-11 11:59:55 -0500

    updated applicants controller file

--- controllers/applicantsController.js
+++ controllers/applicantsController.js
@@ -3,12 +3,16 @@ var express = require('express');
 var router  = express.Router();
 var mysql = require('mysql');
 var path = require('path');
-
 var router = express();
-
 router.get('/home', function(req,res) {
   res.render('users/homepage');
 });
-
+// post request for the new appliant form
+router.post("/create", function(req, res) {
+    var query = "INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id, driver_license, driver_exp, sora_license, sora_exp) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    // Connection to mysql initalized
+    connection.query(query, [req.body.first_name, req.body.last_name, req.body.home_phone, req.body.cell_phone, req.body.email, req.body.contract_id, req.body.position_id, req.body.driver_license, req.body.driver_exp, req.body.sora_license, req.body.sora_exp], function(err, response) {
+            res.redirect('users/homepage')
+        });
+    })
 module.exports = router;
-
