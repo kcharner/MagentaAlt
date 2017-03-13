f41df27e04a21fead6c82e5a923f7abcfdcd3725
Ishani Amin <ishaniamin@Ishanis-MacBook-Pro.local>
2017-03-13 18:17:22 -0400

    updating seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -19,14 +19,14 @@ INSERT INTO positions (position) VALUES ("Team Lead");
 INSERT INTO positions (position) VALUES ("Bus Driver");
 INSERT INTO positions (position) VALUES ("Operations Manager");
 
-INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id) VALUES ("Miriam", "Lugo", "9738908890", "6789876655", "miriam@magentaATS.com", 1, 1);
-INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id) VALUES("John", "Smith", "6548908906", "6549876655", "john@magentaATS.com", 1, 1);
-INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id) VALUES("Anna", "Lugo", "9736890890", "6789878655", "anna@magentaATS.com", 1, 2);
-INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id) VALUES("Andrew", "Reed", "9079792366", "6885546655", "andrew@magentaATS.com", 1, 9);
-INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id) VALUES("Georgia", "Noriega", "9738908890", "6789876655", "georgia@magentaATS.com", 1, 3);
-INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id, driver_license, driver_exp) VALUES("James", "Winkler", "4199069540", "3455616655", "james@magentaATS.com", 3, 4, "MD56758473612", "2019-10-13");
-INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id) VALUES("Kathryn", "Eller", "7135558943", "7135558655", "kathryn@magentaATS.com", 3, 6);
-INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id, driver_license, driver_exp) VALUES("David", "Calvert", "3054308163", "4308163305", "david@magentaATS.com", 3, 4, "NJ5764837563", "2017-05-07");
+INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id) VALUES ("Miriam", "Lugo", "9738908890", "6789876655", "miriam@magentaATS.com", 1, 8);
+INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id) VALUES("John", "Smith", "6548908906", "6549876655", "john@magentaATS.com", 2, 7);
+INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id) VALUES("Anna", "Lugo", "9736890890", "6789878655", "anna@magentaATS.com", 3, 6);
+INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id) VALUES("Andrew", "Reed", "9079792366", "6885546655", "andrew@magentaATS.com", 4, 6);
+INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id) VALUES("Georgia", "Noriega", "9738908890", "6789876655", "georgia@magentaATS.com", 5, 4);
+INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id, driver_license, driver_exp) VALUES("James", "Winkler", "4199069540", "3455616655", "james@magentaATS.com", 6, 3, "MD56758473612", "2019-10-13");
+INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id) VALUES("Kathryn", "Eller", "7135558943", "7135558655", "kathryn@magentaATS.com", 7, 2);
+INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id, driver_license, driver_exp) VALUES("David", "Calvert", "3054308163", "4308163305", "david@magentaATS.com", 1, 1, "NJ5764837563", "2017-05-07");
 
 
 
