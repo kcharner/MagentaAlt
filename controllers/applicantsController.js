5cf094011085d4bc43faaab69b2bab293346645c
Ishani Amin <ishaniamin@Ishanis-MacBook-Pro.local>
2017-03-13 18:52:04 -0400

    add application works

--- controllers/applicantsController.js
+++ controllers/applicantsController.js
@@ -28,7 +28,7 @@ router.post("/create", function(req, res) {
     
             query = "INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id, driver_license, driver_exp, sora_license, sora_exp) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
             
-            connection.query(query, [req.body.first_name, req.body.last_name, req.body.home_phone, req.body.cell_phone, req.body.email, req.body.contract_id, req.body.position_id, req.body.driver_license, req.body.sora_license, req.body.sora_exp], function(error, results, field) {
+            connection.query(query, [req.body.first_name, req.body.last_name, req.body.home_phone, req.body.cell_phone, req.body.email, req.body.contract_id, req.body.position_id, req.body.driver_license, req.body.driver_exp, req.body.sora_license, req.body.sora_exp], function(error, results, field) {
                   console.log("Here's an error!");
                   console.log(error);
                   console.log("End error");
@@ -39,6 +39,7 @@ router.post("/create", function(req, res) {
 
 // post request to view all applicants (add in conditional for whether or not there are open applicants)
 router.post("/viewAll", function(req, res) {
+  
     query = "SELECT a.id, a.first_name, a.last_name, a.cell_phone, a.email, c.contract_name, p.position FROM applicants a LEFT JOIN contracts c ON c.id = a.contract_id LEFT JOIN positions p ON a.position_id = p.id WHERE contract_id = ?";
 
     connection.query(query, [req.body.contract], function(err, response) {
@@ -60,7 +61,7 @@ router.put("/update", function(req, res) {
       query = "SELECT * from applicants where id = ?";
     }
 
-    connection.query("UPDATE applicants SET first_name = ?, SET last_name = ?, SET home_phone = ?, SET cell_phone = ?, SET email = ?, SET contract_id = ?, SET position_id = ?, SET driver_license = ?, SET driver_exp = ?, SET sora_license = ?, SET sora_exp = ? WHERE id = this.id", [req.body.first_name, req.body.last_name, req.body.home_phone, req.body.cell_phone, req.body.email, req.body.contract_id, req.body.position_id, req.body.driver_license, req.body.sora_license, req.body.sora_exp],function(err, result) {
+    connection.query("UPDATE applicants SET first_name = ?, SET last_name = ?, SET home_phone = ?, SET cell_phone = ?, SET email = ?, SET contract_id = ?, SET position_id = ?, SET driver_license = ?, SET driver_exp = ?, SET sora_license = ?, SET sora_exp = ? WHERE id = this.id", [req.body.first_name, req.body.last_name, req.body.home_phone, req.body.cell_phone, req.body.email, req.body.contract_id, req.body.position_id, req.body.driver_license, req.body.driver_exp, req.body.sora_license, req.body.sora_exp],function(err, result) {
           res.redirect("/home");
       });
 });
