c3cb6e22720d10030ca00f2fef7e273beed1d3b2
Ishani Amin <ishaniamin@Ishanis-MacBook-Pro.local>
2017-03-13 20:56:13 -0400

    updating

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,40 +4,106 @@ var router  = express.Router();
 var mysql = require('mysql');
 var path = require('path');
 var router = express();
-var authUsers = ["kristacharner@outlook.com"];
+var connection = require('../config/connection.js')
 
-// NEED CONDITIONAL BASED ON LEVEL (of authority)
-// create array of users = if name is present, can create account??
 
-router.get('/dashboard', function(req,res) {
-  res.render('users/dashboard');
+
+
+
+router.get("/sign-in", function(req, res) {
+  res.sendFile(path.join(__dirname, "../views/sign-in.html"));
 });
 
-// post request for logging in
-// router.post('/login', function(req, res) {
+router.get("/registration", function(req, res) {
+  res.sendFile(path.join(__dirname, "../views/registration.html"));
+});
+
+
+
+
+
+
+
+
+
+router.post('/login', function(req, res) {
   
-//   var query = "SELECT * FROM users WHERE email = ?";
+  // var query = "SELECT * FROM users WHERE email = ?";
 
-//   connection.query(query, [ req.body.email ], function(err, response) {
-//       if (response.length == 0){
-//         res.redirect('/sign-in')
-//       }
+  // connection.query(query, [ req.body.email ], function(err, response) {
+  //     if (response.length == 0){
+  //       res.redirect('/sign-in')
+  //     }
 
-//         bcrypt.compare(req.body.password, response[0].password_hash, function(err, result) {
-//             if (result == true){
+        bcrypt.compare(req.body.password, response[0].password_hash, function(err, result) {
+            if (result == true){
 
-//               req.session.logged_in = true; NEED TO ADD TO USERS TABLE
-//               req.session.id = response[0].id;
-//               req.session.email = response[0].email; 
-//               req.session.first_name = response[0].first_name; GREET USER ON LOG-IN
+              req.session.logged_in = true;
+              // NEED TO ADD TO USERS TABLE
+              req.session.id = response[0].id;
+              req.session.email = response[0].email; 
+              req.session.first_name = response[0].first_name; 
+              // GREET USER ON LOG-IN
 
-//               res.redirect('/home');
-//             }else{
-//               res.redirect('/sign-in')
-//             }
-//         });
-//   });
-// });
+              res.redirect('/home');
+            }else{
+              res.redirect('/sign-in')
+            }
+        });
+  // });
+});
+
+
+
+
+
+
+
+router.post('/create', function(req,res) {
+  // var query = "SELECT * FROM users WHERE email = ?"
+
+  // connection.query(query, [ req.body.email ], function(err, response) {
+    // console.log(response)
+    // if (response.length > 0) {
+    //   res.send('we already have an email for this account')
+    // }else{
+
+      bcrypt.genSalt(10, function(err, salt) {
+          //res.send(salt)
+          bcrypt.hash(req.body.password, salt, function(err, hash) {            
+            var query = "INSERT INTO users (first_name, last_name, email, password, level) VALUES (?, ?, ?, ?, ?)"
+
+            connection.query(query, [ req.body.first_name, req.body.last_name, req.body.email, req.body.password, req.body.level], function(err, response) {
+
+              req.session.logged_in = true;
+
+              req.session.user_id = response.insertId; //only way to get id of an insert for the mysql npm package
+
+              var query = "SELECT * FROM users WHERE id = ?"
+              connection.query(query, [ req.session.user_id ], function(err, response) {
+              	req.session.first_name = response[0].first_name;
+              	req.session.last_name = response[0].last_name;
+                req.session.email = response[0].email;
+                req.session.level = response[0].level;
+
+                res.redirect('/home')
+              });
+            });
+          });
+      });
+
+    // }
+  // });
+
+
+});
+
+
+
+
+
+
+
 
 router.get('/sign-out', function(req,res) {
   req.session.destroy(function(err) {
