b8712418b55ec7f08731564a4c711ab19ad81851
alinton12 <lintonarron@gmail.com>
2017-03-13 23:21:02 -0400

    inserted data into the application_process table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,40 +1,44 @@
 use magenta_db;
 
-INSERT INTO contracts (contract_name, business_unit) VALUES ("Transportation", "75160001");
-INSERT INTO contracts (contract_name, business_unit) VALUES ("Warehouse", "75160006");
-INSERT INTO contracts (contract_name, business_unit) VALUES ("Lav&Water", "75160008");
-INSERT INTO contracts (contract_name, business_unit) VALUES ("FeDex", "75160015");
-INSERT INTO contracts (contract_name, business_unit) VALUES ("VBC", "75160020");
-INSERT INTO contracts (contract_name, business_unit) VALUES ("Wheelchair", "75160022");
-INSERT INTO contracts (contract_name, business_unit) VALUES ("Port Authority Baggage", "75160022");
+-- INSERT INTO contracts (contract_name, business_unit) VALUES ("Transportation", "75160001");
+-- INSERT INTO contracts (contract_name, business_unit) VALUES ("Warehouse", "75160006");
+-- INSERT INTO contracts (contract_name, business_unit) VALUES ("Lav&Water", "75160008");
+-- INSERT INTO contracts (contract_name, business_unit) VALUES ("FeDex", "75160015");
+-- INSERT INTO contracts (contract_name, business_unit) VALUES ("VBC", "75160020");
+-- INSERT INTO contracts (contract_name, business_unit) VALUES ("Wheelchair", "75160022");
+-- INSERT INTO contracts (contract_name, business_unit) VALUES ("Port Authority Baggage", "75160022");
 
 
-INSERT INTO positions (position) VALUES ("Escort");
-INSERT INTO positions (position) VALUES ("Gate Agent");
-INSERT INTO positions (position) VALUES ("Supervisor");
-INSERT INTO positions (position) VALUES ("Lav Driver");
-INSERT INTO positions (position) VALUES ("VBC");
-INSERT INTO positions (position) VALUES ("Dispatcher");
-INSERT INTO positions (position) VALUES ("Wheelchair Agent");
-INSERT INTO positions (position) VALUES ("Baggage Handler");
-INSERT INTO positions (position) VALUES ("Team Lead");
-INSERT INTO positions (position) VALUES ("Bus Driver");
-INSERT INTO positions (position) VALUES ("Operations Manager");
+-- INSERT INTO positions (position) VALUES ("Escort");
+-- INSERT INTO positions (position) VALUES ("Gate Agent");
+-- INSERT INTO positions (position) VALUES ("Supervisor");
+-- INSERT INTO positions (position) VALUES ("Lav Driver");
+-- INSERT INTO positions (position) VALUES ("VBC");
+-- INSERT INTO positions (position) VALUES ("Dispatcher");
+-- INSERT INTO positions (position) VALUES ("Wheelchair Agent");
+-- INSERT INTO positions (position) VALUES ("Baggage Handler");
+-- INSERT INTO positions (position) VALUES ("Team Lead");
+-- INSERT INTO positions (position) VALUES ("Bus Driver");
+-- INSERT INTO positions (position) VALUES ("Operations Manager");
 
-INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id) VALUES ("Miriam", "Lugo", "9738908890", "6789876655", "miriam@magentaATS.com", 1, 8);
-INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id) VALUES("John", "Smith", "6548908906", "6549876655", "john@magentaATS.com", 2, 7);
-INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id) VALUES("Anna", "Lugo", "9736890890", "6789878655", "anna@magentaATS.com", 3, 6);
-INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id) VALUES("Andrew", "Reed", "9079792366", "6885546655", "andrew@magentaATS.com", 4, 6);
-INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id) VALUES("Georgia", "Noriega", "9738908890", "6789876655", "georgia@magentaATS.com", 5, 4);
-INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id, driver_license, driver_exp) VALUES("James", "Winkler", "4199069540", "3455616655", "james@magentaATS.com", 6, 3, "MD56758473612", "2019-10-13");
-INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id) VALUES("Kathryn", "Eller", "7135558943", "7135558655", "kathryn@magentaATS.com", 7, 2);
-INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id, driver_license, driver_exp) VALUES("David", "Calvert", "3054308163", "4308163305", "david@magentaATS.com", 1, 1, "NJ5764837563", "2017-05-07");
-
-
+-- INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id) VALUES ("Miriam", "Lugo", "9738908890", "6789876655", "miriam@magentaATS.com", 1, 8);
+-- INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id) VALUES("John", "Smith", "6548908906", "6549876655", "john@magentaATS.com", 2, 7);
+-- INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id) VALUES("Anna", "Lugo", "9736890890", "6789878655", "anna@magentaATS.com", 3, 6);
+-- INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id) VALUES("Andrew", "Reed", "9079792366", "6885546655", "andrew@magentaATS.com", 4, 6);
+-- INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id) VALUES("Georgia", "Noriega", "9738908890", "6789876655", "georgia@magentaATS.com", 5, 4);
+-- INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id, driver_license, driver_exp) VALUES("James", "Winkler", "4199069540", "3455616655", "james@magentaATS.com", 6, 3, "MD56758473612", "2019-10-13");
+-- INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id) VALUES("Kathryn", "Eller", "7135558943", "7135558655", "kathryn@magentaATS.com", 7, 2);
+-- INSERT INTO applicants (first_name, last_name, home_phone, cell_phone, email, contract_id, position_id, driver_license, driver_exp) VALUES("David", "Calvert", "3054308163", "4308163305", "david@magentaATS.com", 1, 1, "NJ5764837563", "2017-05-07");
 
 
-
-
+INSERT INTO application_process (applicant_id, applied) VALUES (1, "2017-03-13");
+INSERT INTO application_process (applicant_id, applied) VALUES (2, "2017-03-13");
+INSERT INTO application_process (applicant_id, applied) VALUES (3, "2017-03-13");
+INSERT INTO application_process (applicant_id, applied) VALUES (4, "2017-03-13");
+INSERT INTO application_process (applicant_id, applied) VALUES (5, "2017-03-13");
+INSERT INTO application_process (applicant_id, applied) VALUES (6, "2017-03-13");
+INSERT INTO application_process (applicant_id, applied) VALUES (7, "2017-03-13");
+INSERT INTO application_process (applicant_id, applied) VALUES (8, "2017-03-13");
 
 
 
