5255244dd061847fd6093c02c20e93dec94b49f4
Rhynas <rhynas@gmail.com>
2017-03-18 07:49:54 -0400

    Added positions and contracts query

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -3,42 +3,63 @@ var path = require('path');
 var bcrypt = require('bcryptjs');
 var express = require('express');
 var connection = require('../config/connection.js')
-var q = require('q');
+var Q = require('Q');
 var router = express();
 
 router.get('/applicants', function(req,res) {
-
+	//Run query to get the total applicants by Contract
 	function getAppContract(){
-		var defered = q.defer();
-		var queryContracts = "SELECT COUNT(applicants.id) as total_contract, ";
-		queryContracts += " contracts.contract_name as contract, ";
-		queryContracts += " contracts.id as contract_id "
-		queryContracts += " FROM applicants "; 
-		queryContracts += " LEFT JOIN contracts ON applicants.contract_id = contracts.id ";
-		queryContracts += " GROUP BY contract_id;"		
-		connection.query(queryContracts, defered.makeNodeResolver());
+		var defered = Q.defer();
+		var query = "SELECT COUNT(applicants.id) as total_contract, ";
+		query += " contracts.contract_name as contract, ";
+		query += " contracts.id as contract_id "
+		query += " FROM applicants "; 
+		query += " LEFT JOIN contracts ON applicants.contract_id = contracts.id ";
+		query += " GROUP BY contract_id;"		
+		connection.query(query, defered.makeNodeResolver());
 		return defered.promise;
+
 	}
 
+	//Run query to get the total applicants by position
 	function getAppPost(){
-		var defered = q.defer();
-		var queryPositions = "SELECT COUNT(applicants.id) as total_pos, "
-		queryPositions += " positions.id as position_id, ";
-		queryPositions += " positions.position as position, ";
-		queryPositions += " contracts.contract_name as contract ";
-		queryPositions += " FROM applicants "; 
-		queryPositions += " LEFT JOIN contracts ON applicants.contract_id = contracts.id ";
-		queryPositions += " INNER JOIN positions ON applicants.position_id = positions.id ";
-		queryPositions += " GROUP BY position_id;"
-		connection.query(queryPositions, defered.makeNodeResolver());
+		var defered = Q.defer();
+		var query = "SELECT COUNT(applicants.id) as total_pos, "
+		query += " positions.id as position_id, ";
+		query += " positions.position as position, ";
+		query += " contracts.contract_name as contract ";
+		query += " FROM applicants "; 
+		query += " LEFT JOIN contracts ON applicants.contract_id = contracts.id ";
+		query += " INNER JOIN positions ON applicants.position_id = positions.id ";
+		query += " GROUP BY position_id;"
+		connection.query(query, defered.makeNodeResolver());
 		return defered.promise;	
 	}
 
-   q.all([getAppContract(),getAppPost()]).then(function(results){
+	//Run query to get the contracts
+	function getContract(){
+		var defered = Q.defer();
+		var query = "SELECT * "
+		query += " FROM contracts; "; 
+		connection.query(query, defered.makeNodeResolver());
+		return defered.promise;	
+	}
+
+	//Run query to get the total positions
+	function getPosition(){
+		var defered = Q.defer();
+		var query = "SELECT * "
+		query += " FROM positions; "; 
+		connection.query(query, defered.makeNodeResolver());
+		return defered.promise;			
+	}
+
+	//return all queries
+   	Q.all([getAppContract(),getAppPost(), getContract(), getPosition()]).then(function(results){
  		res.json(results);
     });
-
 });
+
 
 router.get('/dashboard', function(req,res) {
     res.render('users/dashboard');
